d28308109262c03fc850975a782708c343010318
peter.biro <peter.biro@oliasoft.com>
2022-09-15 14:37:54 +0200

    create card component

---
+++ src/components/card/card.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import styles from './card.module.css';
+
+export const Card = ({
+  bordered,
+  heading,
+  children,
+  margin,
+  padding,
+  raised,
+}) => {
+  return (
+    <div
+      className={cx(
+        styles.card,
+        bordered ? styles.bordered : '',
+        raised ? styles.raised : '',
+      )}
+      style={{ margin }}
+    >
+      {heading ? <div className={cx(styles.cardHeader)}>{heading}</div> : null}
+      <div
+        className={cx(
+          styles.cardContent,
+          heading ? styles.cardContentBorderTop : '',
+          padding ? styles.padding : '',
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+Card.defaultProps = {
+  bordered: true,
+  heading: null,
+  margin: '0',
+  padding: true,
+  raised: false,
+};
+
+Card.propTypes = {
+  bordered: PropTypes.bool,
+  heading: PropTypes.node,
+  margin: PropTypes.string,
+  padding: PropTypes.bool,
+  raised: PropTypes.bool,
+};
---
+++ src/stories/card.stories.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Card } from '../components/card/card';
+
+export default {
+  title: 'Layout/Card',
+  component: Card,
+
+  args: {
+    raised: false,
+    children: 'Content goes here',
+    padding: true,
+    bordered: true,
+  },
+};
+
+const Template = (args) => (
+  <Card
+    {...args}
+  />
+);
+
+export const Default = Template.bind({});
+
+export const WithHeading = Template.bind({});
+WithHeading.args = {
+  heading: <div>Heading</div>,
+};
+
+export const NoPadding = Template.bind({});
+NoPadding.args = { padding: false };
+
+export const NoBorder = Template.bind({});
+NoBorder.args = { bordered: false };
+
+export const Raised = Template.bind({});
+Raised.args = { raised: true };
+
+export const RaisedWithHeading = Template.bind({});
+RaisedWithHeading.args = { raised: true, heading: <div>Heading</div> };
