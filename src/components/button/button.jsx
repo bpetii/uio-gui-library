db1b89ed6dde469fec596fb404046afe5dd7d7f4
peter.biro <peter.biro@oliasoft.com>
2022-09-15 14:38:11 +0200

    add a button component

---
+++ src/components/button/button.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import buttonStyles from './button.module.css';
+
+const Button = ({
+  active,
+  basic,
+  colored,
+  disabled,
+  label,
+  name,
+  onClick,
+  pill,
+  round,
+  small,
+  width,
+  styles,
+}) => {
+
+  const color = (() => {
+    if (colored) {
+      switch (colored) {
+        case 'danger':
+        case 'red':
+          return buttonStyles.red;
+        case 'success':
+        case 'green':
+          return buttonStyles.green;
+        case 'muted':
+          return buttonStyles.muted;
+        default:
+          return buttonStyles.blue;
+      }
+    }
+    return '';
+  })();
+
+
+  return (
+    <button
+      type="button"
+      className={cx(
+        buttonStyles.button,
+        disabled ? buttonStyles.disabled : '',
+        basic ? buttonStyles.basic : '',
+        small ? buttonStyles.small : '',
+        active ? buttonStyles.active : '',
+        basic ? buttonStyles.basic : '',
+        color,
+        pill ? buttonStyles.pill : '',
+        round ? buttonStyles.round : '',
+        styles  
+      )}
+      disabled={disabled}
+      name={name}
+      onClick={onClick}
+      style={{ width }}
+    >
+      {label}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  active: false,
+  basic: false,
+  colored: false,
+  disabled: false,
+  label: '',
+  name: undefined,
+  pill: false,
+  round: false,
+  small: false,
+  styles: '',
+  width: '',
+  onClick: () => {},
+};
+
+Button.propTypes = {
+  active: PropTypes.bool,
+  basic: PropTypes.bool,
+  colored: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  disabled: PropTypes.bool,
+  label: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
+  name: PropTypes.string,
+  onClick: PropTypes.func,
+  round: PropTypes.bool,
+  small: PropTypes.bool,
+  styles: PropTypes.string,
+  width: PropTypes.string,
+  pill: PropTypes.bool,
+};
+
+export default Button
---
+++ src/stories/button.stories.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { FaTrash } from 'react-icons/fa';
+import Button from '../components/button/button';
+
+export default {
+  title: 'Forms/Button',
+  component: Button,
+  argTypes: {
+    colored: {
+      control: {
+        type: 'inline-radio',
+      },
+      options: {
+        off: false,
+        primary: 'primary',
+        success: 'success',
+        danger: 'danger',
+        muted: 'muted',
+      },
+    },
+    icon: {
+      control: { type: 'inline-radio' },
+      options: {
+        off: undefined,
+        on: <FaTrash />,
+      },
+    },
+  },
+  args: {
+    label: 'Label',
+    colored: false,
+    disabled: false,
+    basic: false,
+  },
+};
+
+const Template = (args) => (
+  <Button
+    {...args}
+  />
+);
+
+export const Default = Template.bind({});
+
+export const Small = Template.bind({});
+Small.args = { small: true };
+
+export const Basic = Template.bind({});
+Basic.args = { basic: true };
+
+export const Active = Template.bind({});
+Basic.args = { active: true };
+
+export const Round = Template.bind({});
+Round.args = { round: true, label: <FaTrash /> };
+
+export const Disabled = Template.bind({});
+Disabled.args = { disabled: true };
+
+export const FullWidth = Template.bind({});
+FullWidth.args = { width: '100%' };
