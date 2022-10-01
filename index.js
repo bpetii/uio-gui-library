0f29d6335d2bcaed91458ea14d15de86116784c8
peter.biro <peter.biro@oliasoft.com>
2022-10-01 11:39:20 +0200

    export all components as an external npm package

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ export {Badge} from './src/components/badge/badge';
 export {Button} from './src/components/button/button';
 export {Card} from './src/components/card/card';
 export {CheckBox} from './src/components/checkbox/checkbox';
+export {Divider } from './src/components/divider/divider';
 export {Field} from './src/components/field/field';
+export {Flex} from './src/components/flex/flex';
+export {Icon} from './src/components/icon/icon';
+export {Input} from './src/components/input/input';
+export {Label} from './src/components/label/label';
+export {Loader} from './src/components/loader/loader';
+export {CustomSelect} from './src/components/select/customSelect';
+export {Spacer} from './src/components/spacer/spacer';
+export {Spinner} from './src/components/spinner/spinner';
+export {Table} from './src/components/table/table';
+export {Toggle} from './src/components/toggle/toggle';
  
  
