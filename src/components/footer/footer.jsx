9b03251ffd1c24699a84373ac2c29c75ab6b4a18
peter.biro <peter.biro@oliasoft.com>
2022-10-26 18:12:11 +0200

    fix footer dark mode v2

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
     <div className={styles.footer}>
       <Flex direction="column" alignItems="center">
         <Spacer height="15px" />
-        <Text >
+        <Text color='var(--color-footer-text-color)'>
           &copy; {new Date().getFullYear()} Mohammedreza Dianat and Péter Bíró
         </Text>
         <Spacer height="10px" />
