daaef02f33548924412a00c28b9595cc2b26c35d
peter.biro <peter.biro@oliasoft.com>
2022-11-07 22:46:50 +0100

    remove dead dependency from rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import {babel} from '@rollup/plugin-babel';
 import autoprefixer from 'autoprefixer';
-import simpleVars from 'postcss-simple-vars';
 import requireContext from 'rollup-plugin-require-context';
-import postcss from 'rollup-plugin-postcss'
 import styles from 'rollup-plugin-styles';
-import embedCSS from 'rollup-plugin-embed-css';
 
 const packageJson = require("./package.json");
 
